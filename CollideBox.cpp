9186e1a4083e91fb56b698809261f1de8b5c872a
Chris Langford <chris@hellocld.com>
2012-08-22 15:58:51 -0400

    Updated to utilize Vector2D

--- CollideBox.cpp
+++ CollideBox.cpp
@@ -1,31 +1,71 @@
 #include "CollideBox.h"
+#include "Vector2D.h"
 
 using namespace std;
 
-CollideBox::CollideBox(float width, float height, float x, float y)
+CollideBox::CollideBox(float width, float height, float x, float y, float xvel, float yvel)
 {
-	this.width = width;
-	this.height = height;
-	this.x = x;
-	this.y = y;
+	Position = new Vector2D(x, y);
+	Dimensions = new Vector2D(width, height);
+	Velocity = new Vector2D(xvel, yvel);
 }
 
 float CollideBox::GetWidth()
 {
-	return width;
+	return Dimensions.GetX(); 
 }
 
 float CollideBox::GetHeight()
 {
-	return height;
+	return Dimensions.GetY();
 }
 
 float CollideBox::GetX()
 {
-	return x;
+	return Position.GetX();
 }
 
 float CollideBox::GetY()
 {
-	return y;
+	return Position.GetY();
+}
+
+float CollideBox::GetXvel()
+{
+	return Velocity.GetX();
+}
+
+float CollideBox::GetYvel()
+{
+	return Velocity.GetY();
+}
+
+void CollideBox::SetDimensions(float x, float y)
+{	
+	Dimensions.SetVector(x, y);
+}
+
+void CollideBox::SetPosition(float x, float y)
+{
+	Position.SetVector(x, y);
+}
+
+void CollideBox::SetVelocity(float x, float y)
+{
+	Velocity.SetVector(x, y);
+}
+
+Vector2D CollideBox::GetDimensions()
+{
+	return Dimensions;
+}
+
+Vector2D CollideBox::GetPosition()
+{
+	return Position;
+}
+
+Vector2D CollideBox::GetVelocity()
+{
+	return Velocity;
 }
--- CollideBox.h
+++ CollideBox.h
@@ -4,17 +4,24 @@
 class CollideBox()
 {
 	private:
-		float width;
-		float height;
-		float x;
-		float y;
+		Vector2D Position;
+		Vector2D Dimensions;
+		Vector2D Velocity;
 
 	public:
-		CollideBox(float width, float height, float x, float y);
+		CollideBox(float width, float height, float x, float y, float Xvel, float Yvel);
 		float GetWidth();
 		float GetHeight();
 		float GetX();
 		float GetY();
+		float GetXvel();
+		float GetYvel();
+		void SetDimensions(float x, float y);
+		void SetPosition(float x, float y);
+		void SetVelocity(float x, float y);
+		Vector2D GetPosition();
+		Vector2D GetDimensions();
+		Vector2D GetVelocity();
 };
 
 #endif
